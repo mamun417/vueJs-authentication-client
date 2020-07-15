f21d312b50796c579eff6231602a03a8d78c0ec7
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-07-15 21:06:29 +0600

    show dynamic data of LeftSidebar

--- src/views/element/LeftSidebar.vue
+++ src/views/element/LeftSidebar.vue
@@ -7,8 +7,8 @@
                 <img src="assets/images/user.png" width="48" height="48" alt="User" />
             </div>
             <div class="info-container">
-                <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
-                <div class="email">john.doe@example.com</div>
+                <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ userInfo.name }}</div>
+                <div class="email">{{ userInfo.email }}</div>
                 <div class="btn-group user-helper-dropdown">
                     <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                     <ul class="dropdown-menu pull-right">
@@ -50,3 +50,18 @@
     </aside>
     <!-- #END# Left Sidebar -->
 </template>
+
+
+<script>
+    import {mapGetters} from "vuex";
+
+    export default {
+        name: 'LeftSideBar',
+        
+        computed: {
+            ...mapGetters({
+                userInfo : 'auth/user'
+            })
+        }
+    }
+</script>
